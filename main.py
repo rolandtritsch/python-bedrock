caf0ea1ef9c402968c6702bcdb0cc54d7e54fb03
Roland Tritsch <roland@tritsch.email>
2025-06-14 13:20:07 +0100

    Add get_model()

--- main.py
+++ main.py
@@ -3,16 +3,25 @@
 import boto3 as aws
 import json
 
+bedrock = aws.client("bedrock")
 
 def list_models():
-    bedrock = aws.client("bedrock")
     models = bedrock.list_foundation_models()
     return models
+
+def get_model(model: str):
+    model = bedrock.get_foundation_model(modelIdentifier=model)
+    return model["modelDetails"]
 
 
 def main():
     models = list_models()
     print(json.dumps(models, default=str))
+
+    print('\n---\n')
+    
+    model = get_model('amazon.titan-text-lite-v1')
+    print(json.dumps(model, default=str))
 
 
 if __name__ == "__main__":
