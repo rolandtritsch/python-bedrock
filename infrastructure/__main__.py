f48d1b265ea837824c870ffab1d65e2ebd6743eb
Roland Tritsch <roland@tritsch.email>
2025-06-16 06:55:15 +0100

    Remove infrastructure (for now)

--- infrastructure/__main__.py
+++
@@ -1,8 +0,0 @@
-"""The AWS Bedrock Playground Resources"""
-
-import pulumi
-import pulumi_aws as aws
-
-bucket = aws.s3.BucketV2("aws-bedrock-playground-bucket")
-
-pulumi.export("bucket_name", bucket.id)
